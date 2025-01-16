01d45a2f5f22edeedc4ce07ba1452ccd40e1b95a
LukeBravermanCityUniversity <luke.braverman.2@city.ac.uk>
2025-01-16 02:03:26 +0000

    added http calls for data

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,19 +1,55 @@
 import './Sidebar.css';
 import Switch from "@mui/material/Switch";
+import {useEffect, useState} from "react";
 
 
 function Sidebar()
-
     {
+        const SERVER_IP: string = '127.0.0.1';
         const label = { inputProps: { 'aria-label': 'Switch demo' } };
+        const [isBackendAlive, setIsBackendAlive] = useState(true);
+
+        useEffect(() => {
+            const checkBackend = () => {
+                console.log('sending heartbeat!')
+                fetch(`http://${SERVER_IP}:8080/health`)
+                    .then(response => {
+                        if (response.ok) {
+                            console.log('heartbeat successful!')
+                            setIsBackendAlive(true);
+                        } else {
+                            console.log('heartbeat failed.. trying again in 5 seconds.')
+                            setIsBackendAlive(false);
+                        }
+                    })
+                    .catch(() => setIsBackendAlive(false));
+            };
+
+            // Check backend every 5 seconds
+            const intervalId = setInterval(checkBackend, 5000);
+
+            // Cleanup interval on component unmount
+            return () => clearInterval(intervalId);
+        }, []);
 
         return (
 
             <div className="sidebar">
                 <p>Packet Viewer App</p>
                 <div className="connectionTest">
-                    <p>Connected</p>
-                    <Switch {...label} disabled defaultChecked />
+                    {isBackendAlive &&
+                        (
+                            <>
+                                <p>Connected</p>
+                                <Switch {...label} disabled defaultChecked/>
+                            </>
+                        )}
+                    {!isBackendAlive && (
+                        <>
+                            <p>Connecting...</p>
+                            <Switch {...label} disabled  />
+                        </>
+                    )}
                 </div>
 
             </div>
--- src/components/mainview/MainView.tsx
+++ src/components/mainview/MainView.tsx
@@ -1,9 +1,28 @@
 import './MainView.css'
 import Logcard from "../logcard/Logcard.tsx";
+import {useEffect} from "react";
 
 function MainView()
 {
 
+    const SERVER_IP: string = '127.0.0.1';
+
+    useEffect(() => {
+        const returnLogs = () => {
+            console.log('sending heartbeat!')
+            fetch(`http://${SERVER_IP}:8080/logs`)
+                .then(response => {
+                    console.log('logs found', response);
+                })
+                .catch(() => {});
+        };
+
+        // Check backend every 5 seconds
+        const intervalId = setInterval(returnLogs, 5000);
+
+        // Cleanup interval on component unmount
+        return () => clearInterval(intervalId);
+    });
 
     return (
 
